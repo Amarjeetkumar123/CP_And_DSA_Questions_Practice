83bf57c1eeefad24d85a4d08a83116ba921d78d5
Amarjeet Kumar <amarjeetkumar802212@gmail.com>
2022-07-06 15:54:18 +0530

    first_day

---
+++ LInearKeyboard.cpp
@@ -0,0 +1,33 @@
+// Question Link : https://codeforces.com/problemset/problem/1607/A
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		string keyboard, word;
+		cin >> keyboard >> word;
+
+		int mapping[256] = {0};
+
+		for (int i = 0; i < 26; i++) {
+			mapping[keyboard[i]] = i + 1;
+		}
+
+
+		int ans = 0;
+
+		for (int i = 0; i < word.length() - 1; i++) {
+			ans += abs(mapping[word[i + 1]] - mapping[word[i]]);
+		}
+
+		cout << ans << endl;
+	}
+    
+    return 0;
+}
+
+
+
