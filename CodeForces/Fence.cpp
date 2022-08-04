34119996472826cc5995b417c533f1cd1c0a9eaf
Amarjeet Kumar <amarjeetkumar802212@gmail.com>
2022-08-04 10:19:17 +0530

    CodeForces

---
+++ CodeForces/Fence.cpp
@@ -0,0 +1,41 @@
+// Question Link : https://codeforces.com/contest/363/problem/B
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n, k;
+
+    cin >> n >> k;
+    vector<int> arr(n + 1);
+
+    for (int i = 1; i <= n; i++)
+        cin >> arr[i];
+
+        int ans = 1;
+    int sum = 0;
+
+    for (int i = 1; i <= k; i++)
+        sum += arr[i];
+
+    int Min = sum;
+
+    // slidding window
+    int l = 0, r = k;
+
+    while(r < n){
+        l++;
+        r++;
+        sum -= arr[l];
+        sum += arr[r];
+        if(sum < Min){
+            Min = sum;
+            ans = l+1;
+        }
+    }
+    cout << ans << endl;
+
+    return 0;
+}
