f42d16e32101e228e714cb74b6e7652d34310957
Amarjeet Kumar <amarjeetkumar802212@gmail.com>
2022-11-05 12:19:47 +0530

    initial

---
+++ GFG/POTD_gfg/December/test.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void){
+ios::sync_with_stdio(0);
+cin.tie(0);
+cout.tie(0);
+
+int t = 1;
+//cin>>t;
+
+while(t--){
+
+
+}
+    return 0;
+}
